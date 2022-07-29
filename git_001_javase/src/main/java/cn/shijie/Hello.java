4b2c57608db7fde3f2218c6fdbd78886378f20ce
HelloXiu <68998926+HelloXiu@users.noreply.github.com>
2022-07-29 16:02:25 +0800

    提交喽

---
+++ git_001_javase/src/main/java/cn/shijie/Hello.java
@@ -0,0 +1,7 @@
+package cn.shijie;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("这是JavaSe。。。。。");
+    }
+}
---
+++ git_002_web/src/main/java/cn/shijie/controller/DemoController.java
@@ -0,0 +1,7 @@
+package cn.shijie.controller;
+
+public class DemoController {
+    public static void main(String[] args) {
+        System.out.println("这是web项目。。。。。。。。。。");
+    }
+}
